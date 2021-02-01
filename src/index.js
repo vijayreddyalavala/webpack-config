b02a3765780e7a38f9939989b91064ca64cc637d
vijayreddyalavala <vijay.alavala@gmail.com>
2021-02-02 01:23:41 +0530

    Adding Babel to Project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elevenShieldRecipe = {
 	refineMoonstone: 4
 }
 
+
+const elvenGountletsRecipe =    {
+	...elevenShieldRecipe,
+	leather:1,
+	refineMoonstone: 32
+}
 console.log(elevenShieldRecipe);
+console.log(elvenGountletsRecipe);
